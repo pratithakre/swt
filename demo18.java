8c170878754efc80baca11eac3a3a1670933e482
pratithakre <pratithakre072@gmail.com>
2024-02-27 18:53:24 +0530

    hghg

---
+++ demo18.java
@@ -0,0 +1,18 @@
+ class demo18
+{
+ public static void main(String arg[])
+ {
+    int i,j,c;
+	for(i=2;i<=100;i++)
+	{
+		c=0;
+		for(j=1;j<=i;j++)
+	  {
+		   if(i%j==0)
+		   {
+			    c=1;
+	}}
+	  if(c==2)
+	  {
+         System.out.println(" "+i);
+	}	}}}
